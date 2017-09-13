4a7d1990e131bad48e022af53ecb1754e4a183a5
b04342a <Ludmila.Savova@experian.com>
2017-09-13 17:56:19 +0300

    ANA-4496 Adding steps in DBTest deature to test the DBOperations library with postgre. Adding configuration classes from Colllections framework so as condigurations can be referenced within the config.properties file

---
+++ cucumber/src/main/java/com/experian/common/config/CustomConversionHandler.java
@@ -0,0 +1,21 @@
+package com.experian.common.config;
+
+import org.apache.commons.configuration2.convert.DefaultConversionHandler;
+import org.apache.commons.configuration2.interpol.ConfigurationInterpolator;
+
+import java.util.ArrayList;
+
+public class CustomConversionHandler extends DefaultConversionHandler {
+
+    public <T> T to(Object src, Class<T> targetCls, ConfigurationInterpolator ci) {
+
+        if ( src.getClass().equals(ArrayList.class)){
+            ArrayList list = (ArrayList) src;
+            //src = list.get(list.size() - 1);
+            src = list.get(0);
+        }
+
+        return super.to(src, targetCls, ci);
+    }
+
+}
