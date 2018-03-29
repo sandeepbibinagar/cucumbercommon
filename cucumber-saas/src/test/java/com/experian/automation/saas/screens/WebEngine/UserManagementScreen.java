ea05d2be1241100b4846a5378661d46059093d80
Choi, Yun Yi <YunYi.Choi@experian.com>
2018-03-29 21:30:04 +0800

    QUAL-1349 add wait for 'selectTab' in UserManagementScreen

--- cucumber-saas/src/test/java/com/experian/automation/saas/screens/WebEngine/UserManagementScreen.java
+++ cucumber-saas/src/test/java/com/experian/automation/saas/screens/WebEngine/UserManagementScreen.java
@@ -42,6 +42,7 @@ public class UserManagementScreen extends Screen {
   }
 
   public void selectTab(String tab) {
+    waitForElements(tabsList);
     clickWithScrollToView(getElementByText(tabsList, tab));
   }
 
